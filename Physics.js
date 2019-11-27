8e02fdc1c0e936f7e007b913766935ced79c1483
Jxnis <janis.ellerbrock@gmail.com>
2019-11-27 17:42:48 +0000

    adds ceiling, bottom and touch force to santa

--- Physics.js
+++ Physics.js
@@ -2,7 +2,12 @@ import Matter from 'matter-js';
 
 const Physics = (entities, { touches, time }) => {
     let engine = entities.physics.engine;
+    let santa = entities.santa.body;
 
+    touches.filter(t => t.type === "press").forEach(t => {
+        Matter.Body.applyForce(santa, santa.position, { x: 0.0, y: -0.1 });
+    });
+ 
     Matter.Engine.update(engine, time.delta);
 
     return entities;
