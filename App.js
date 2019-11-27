3359d8e249808edc104db4ac62ae4da2b14d1cbb
Jxnis <janis.ellerbrock@gmail.com>
2019-11-27 19:08:23 +0000

    adds  pipes and sets sizes

--- App.js
+++ App.js
@@ -7,6 +7,21 @@ import Santa from './Santa.js';
 import Physics from './Physics.js';
 import Wall from './Wall.js';
 
+export const randomBetween = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const generatePipes = () => {
+  let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
+  let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
+
+  let sizes = [topPipeHeight, bottomPipeHeight];
+
+  if (Math.random() < 0.5) {
+    sizes = sizes.reverse();
+  }
+  return sizes;
+}
 
 export default class App extends Component {
     constructor(props){
@@ -23,13 +38,26 @@ export default class App extends Component {
       let floor = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 25, Constants.MAX_WIDTH, 50, { isStatic: true });
       let ceiling = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true });
 
-      Matter.World.add(world, [santa, floor]);
+      let [pipe1Height, pipe2Height] = generatePipes();
+      let pipe1 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, {isStatic: true });
+      let pipe2 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, {isStatic: true });
+
+      let [pipe3Height, pipe4Height] = generatePipes();
+      let pipe3 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), pipe3Height / 2, Constants.PIPE_WIDTH, pipe3Height, {isStatic: true });
+      let pipe4 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, { isStatic: true});
+
+      Matter.World.add(world, [santa, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
 
       return {
         physics: {engine: engine, world: world },
         santa: { body: santa, size: [50, 50], color: 'red', renderer: Santa },
         floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
-        ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall }
+        ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
+        pipe1: { body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: 'green', renderer: Wall },
+        pipe2: { body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: 'green', renderer: Wall },
+        pipe3: { body: pipe3, size: [Constants.PIPE_WIDTH, pipe3Height], color: 'green', renderer: Wall },
+        pipe4: { body: pipe4, size: [Constants.PIPE_WIDTH, pipe4Height], color: 'green', renderer: Wall },
+
       }
     }
   
