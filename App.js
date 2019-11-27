ddfa3538c4a235c559be3bbe5c9c3ef33fb6b6b3
Jxnis <janis.ellerbrock@gmail.com>
2019-11-27 16:50:49 +0000

    adds Physics to the app, adds Santa, adds components santa, physics and Constants

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar, Alert, TouchableOpacity } from 'react-native';
+import Constants from './Constants';
+import { GameEngine } from 'react-native-game-engine';
+import Matter from 'matter-js';
+import Santa from './Santa.js';
+import Physics from './Physics.js';
 
-export default function App() {
+
+export default class App extends Component {
+    constructor(props){
+      super(props);
+      this.GameEngine = null;
+      this.entities = this.setupWorld();  
+    }
+  
+    setupWorld = () => {
+      let engine = Matter.Engine.create({ enableSleeping: false });
+      let world = engine.world;
+
+      let santa = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
+
+      Matter.World.add(world, [santa]);
+
+      return {
+        physics: {engine: engine, world: world },
+        santa: { body: santa, size: [50, 50], color: 'red', renderer: Santa }
+      }
+    }
+  
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <GameEngine
+          ref={(ref) => { this.GameEngine = ref; }}
+          style={styles.gameContainer}
+          systems={[Physics]}
+          entities={this.entities} />
+
     </View>
-  );
+  )
 }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    
+  }
 });
