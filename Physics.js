28912d72aef131b29b78d7f748af9e57e128519a
Jxnis <janis.ellerbrock@gmail.com>
2019-11-28 17:16:18 +0000

    adds Images to app, fixes force, adds moving background

--- Physics.js
+++ Physics.js
@@ -3,21 +3,36 @@ import Constants from './Constants';
 
 const Physics = (entities, { touches, time }) => {
     let engine = entities.physics.engine;
+    let world = entities.physics.world;
     let santa = entities.santa.body;
 
+    let hadTouches = false;
     touches.filter(t => t.type === "press").forEach(t => {
-        Matter.Body.applyForce(santa, santa.position, { x: 0.0, y: -0.1 });
+        if (!hadTouches) {
+            if (world.gravity.y === 0.0){
+                world.gravity.y = 1.2;
+            }
+
+            hadTouches = true;
+            Matter.Body.setVelocity( santa, {
+            x: santa.velocity.x,
+            y: -10    
+        });
+      }
     });
 
-    for(let i=1; i<=4; i++){
-        if (entities["pipe" + i].body.position.x <= -1 * (Constants.PIPE_WIDTH / 2)){
-            Matter.Body.setPosition(entities["pipe" + i].body, { x: Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), y: entities["pipe" + i].body.position.y})
-        } else {
-        Matter.Body.translate(entities["pipe" + i].body, { x: -1, y: 0 })
-    }
-} 
  
     Matter.Engine.update(engine, time.delta);
+
+    Object.keys(entities).forEach(key => {
+        if (key.indexOf("floor") === 0) {
+            if (entities[key].body.position.x <= -1 * Constants.MAX_WIDTH / 2) {
+                Matter.Body.setPosition(entities[key].body, { x: Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2), y: entities[key].body.position.y})
+            } else {
+                Matter.Body.translate(entities[key].body, {x: -2, y: 0});   
+        }
+      }
+    })
 
     return entities;
 }
---
+++ assets/Images.js
@@ -0,0 +1,7 @@
+export default Images = {
+    background: require('./png/BG.png'),
+    floor: require('./png/floor.png'),
+    treeTop: require('./png/tree_reverse.png'),
+    treeBottom: require('./png/tree_reverse.png'),
+    character: require('./png/character.png'),
+}
